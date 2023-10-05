73baf54211a31ef3c8af5610134b541565f0ed99
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2023-10-05 15:51:32 +0300

    add folder with non-uft8 filename

--- data/disk-files/bad-filename/create.py
+++ data/disk-files/bad-filename/create.py
@@ -1,4 +1,15 @@
+import os
+
 broken_name = 'modifi\udce9.txt'
+broken_folder = 'folder_' + broken_name
+broken_path = os.path.join(broken_folder, 'file_under_bad_folder_name')
+
+os.makedirs(broken_folder, exist_ok=True)
+
 with open(broken_name, 'w') as f:
     f.write('hello world\n')
     f.write('this is a broken filename with non-unicode entry\n')
+
+with open(broken_path, 'w') as f:
+    f.write('hello world 2\n')
+    f.write('this is a filename inside a folder with a non-utf8 path\n')
